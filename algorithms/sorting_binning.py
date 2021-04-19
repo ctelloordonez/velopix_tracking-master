72095458ad95f682083fd1e546d92b0e54fc3a40
maximeduchateau <69209106+maximeduchateau@users.noreply.github.com>
2021-04-19 22:51:54 +0200

    sorting and binning

--- algorithms/sorting_binning.py
+++ algorithms/sorting_binning.py
@@ -39,16 +39,43 @@ def get_events_data_from_folder(data_set_folder, num_events = -1, shuffle = Fals
             print(f'closing : {filename}')
     return events
 
-
-def sort_by_x():
+#method that takes as input the parameter on which it has to sort hits
+#the possible parameters are 'x','y','z','pol_r','pol_phi'
+#this happens in ascending order
+def sort_by(parameter):
     dataset = 'bsphiphi'
     events = get_events_data_from_folder(dataset_folder[dataset], num_events=10, shuffle=False)
     events_sorted = []
     for event in events:
-        events_sorted.append(sorted(event.hits, key=attrgetter('x', 'y')))
+        if parameter == 'pol_r' or parameter == 'pol_phi':
+            for _ in event.hits:
+                _.update_polar()
+        events_sorted.append(sorted(event.hits, key=attrgetter(parameter)))
     return events_sorted
 
+#method that takes as input the required amount of bins,
+#and the parameter on which it sorts and bins the hits
+#this method starts by calling the sorting method above
 
+def get_bins(bins_count,parameter):
+    events= sort_by(parameter)
+    all_events_bins=[]
+    for event in events:
+        print(event)
+        number_of_hits_of_event=len(event)
+        print(len(event))
+        bins= [[] for i in range(bins_count)]
+        for index in range(len(event)):
+            for b,bin in enumerate(bins):
+                if b*number_of_hits_of_event/bins_count<index<(b + 1)*number_of_hits_of_event/bins_count:
+                    bin.append(event[index])
+        all_events_bins.append(bins)
+    return all_events_bins
+
+
+get_bins(4,'y')
+
+'''
 def sort_by_y():
     dataset = 'bsphiphi'
     events = get_events_data_from_folder(dataset_folder[dataset], num_events=10, shuffle=False)
@@ -74,7 +101,6 @@ def sort_by_pol_r():
     for event in events:
         for _ in event.hits:
             _.update_polar()
-        print(sorted(event.hits, key=attrgetter('pol_r','z')))
         events_sorted.append(sorted(event.hits, key=attrgetter('pol_r','z')))
     return events_sorted
 
@@ -86,7 +112,6 @@ def sort_by_pol_phi():
     for event in events:
         for _ in event.hits:
             _.update_polar()
-        print(sorted(event.hits, key=attrgetter('pol_phi','z')))
         events_sorted.append(sorted(event.hits, key=attrgetter('pol_phi','z')))
     return events_sorted
-
+'''
