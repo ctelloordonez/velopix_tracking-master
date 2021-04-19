6820c40f3a3984c861faccd3b593462c5c14640b
Carlos Tello Ordoñez <charlietello97@gmail.com>
2021-04-19 14:49:21 +0200

    Add coloured tracks in phi plots and phi by z plot

--- visual/plot_phi.py
+++ visual/plot_phi.py
@@ -10,7 +10,7 @@ from algorithms.track_following import track_following
 solutions = {}
 
 # Get an event
-f = open("../events/small_dataset/velo_event_0.json")
+f = open("../events/minibias/velo_event_0.json")
 json_data = json.loads(f.read())
 event = em.event(json_data)
 f.close()
@@ -18,19 +18,29 @@ f.close()
 for s_number in range(0, event.number_of_modules):
   event.modules[s_number].z = event.modules[s_number].hits()[0].z
 
-# Solve with the classic method
-track_following = track_following()
-solutions["classic"] = track_following.solve(event)
+# # Solve with the classic method
+# track_following = track_following()
+# solutions["classic"] = track_following.solve(event)
+#
+# # Solve with the DFS method
+# dfs = graph_dfs(
+#   allow_cross_track=False,
+#   allowed_skip_modules=1,
+#   max_slopes=(0.7, 0.7),
+#   max_tolerance=(0.3, 0.3)
+# )
+# solutions["dfs"] = dfs.solve(event)
 
-# Solve with the DFS method
-dfs = graph_dfs(
-  allow_cross_track=False,
-  allowed_skip_modules=1,
-  max_slopes=(0.7, 0.7),
-  max_tolerance=(0.3, 0.3)
-)
-solutions["dfs"] = dfs.solve(event)
+from visual.print_phi import print_event_2d_phi, print_event_3d_phi, print_event_2d_2phi
+from data_analysis.events_analysis import tracks_from_data
 
-from visual.print_phi import print_event_2d_phi
-print_event_2d_phi(event, solutions["classic"], filename="event_phi_classic")
-print_event_2d_phi(event, solutions["dfs"], filename="event_phi_dfs")
+tracks = tracks_from_data(json_data, only_reconstructible=True)
+print_event_2d_phi(event, tracks, x=2, filename="minibias event0 phi by z", save_to_file=True)
+# print_event_2d_2phi(event, tracks, filename="xz-xy")
+# print_event_2d_phi(event, [], x=1, filename="empty_phi_y")
+# print_event_2d_phi(event, [], filename="empty_phi_z")
+
+# print_event_3d_phi(event, tracks, x=0, y=1, filename='empty3D_phi_xy')
+# print_event_3d_phi(event, tracks, x=0, y=2, rotate=True, filename='empty3D_phi_xz')
+# print_event_3d_phi(event, tracks, x=1, y=2, filename='empty3D_phi_yz')
+# print_event_2d_phi(event, solutions["dfs"], filename="event_phi_dfs")
--- visual/print_phi.py
+++ visual/print_phi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import matplotlib as mpl
-mpl.use('Agg')
+# mpl.use('Agg')
 
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.patches as mpatches
@@ -30,29 +30,58 @@ plotscale = 1.5
 
 ntox = {0:'X', 1:'Y', 2:'Z'}
 
-def hit_phi(hit):
-  if (hit.module_number % 2) == 0:
-    phi = math.atan2(hit.y, hit.x)
-    less_than_zero = phi < 0
-    return phi + less_than_zero * 2 * math.pi
-  return math.atan2(hit.y, hit.x)
+# def hit_phi(hit):
+#   if (hit.module_number % 2) == 0:
+#     phi = math.atan2(hit.y, hit.x)
+#     less_than_zero = phi < 0
+#     return phi + less_than_zero * 2 * math.pi
+#   return math.atan2(hit.y, hit.x)
 
 
-def print_event_2d_phi(event, tracks=[], track_color=0, filename="event_phi"):  
+def hit_phi(x, y):
+    return math.atan2(y, x)
+
+
+def print_event_2d_2phi(event, tracks, phix=0, phiy=0, filename="doublephi", save_to_file=False):
+  fig = plt.figure(figsize=(16*plotscale, 9*plotscale))
+  ax = plt.axes()
+
+  count = 0
+  for t in [t for t in tracks if len(t.hits)>=3]:
+    count+=1
+    plt.scatter(
+      [hit_phi(h[phix], h[phiy]) for h in t.hits],
+      [hit_phi(h[phix], h[phiy]) for h in t.hits],
+      color=colors[count % len(colors)],
+      s=2*scale
+    )
+
+  plt.tick_params(axis='both', which='major', labelsize=4*scale)
+  plt.xlabel("rz", fontdict={'fontsize': 4*scale})
+  plt.ylabel("xy", fontdict={'fontsize': 4*scale}, rotation='horizontal')
+  plt.xlim([-2,2])
+  if save_to_file:
+    plt.savefig(filename + ".png", bbox_inches='tight', pad_inches=0.2)
+    plt.close()
+  else:
+    plt.show()
+
+
+def print_event_2d_phi(event, tracks, x=2, track_color=0, phix=0, phiy=1, filename="event_phi", save_to_file=False):
   fig = plt.figure(figsize=(16*plotscale, 9*plotscale))
   ax = plt.axes()
 
   # Limits of the modules
-  limits = [(0, 2 * math.pi), (-math.pi, math.pi)]
+  limits = [(-math.pi, math.pi), (-math.pi, math.pi)]
   shift = 0.4
-  # for s in event.modules[::2]:
-  #   plt.plot(
-  #     [s.z+shift, s.z+shift],
-  #     [limits[0][0], limits[0][1]],
-  #     color=grey_color,
-  #     alpha=0.4,
-  #     linewidth=4
-  #   )
+  for s in event.modules[::2]:
+    plt.plot(
+      [s.z+shift, s.z+shift],
+      [limits[0][0], limits[0][1]],
+      color=grey_color,
+      alpha=0.4,
+      linewidth=4
+    )
 
   for s in event.modules[1::2]:
     plt.plot(
@@ -63,27 +92,88 @@ def print_event_2d_phi(event, tracks=[], track_color=0, filename="event_phi"):
       linewidth=4
     )
 
-  # Print X versus Phi  # TODO: Print "X" or "Z"?
-  plt.scatter(
-    [h[2] for h in event.hits if h.module_number % 2 == 1],
-    [hit_phi(h) for h in event.hits if h.module_number % 2 == 1],
+  if len(tracks) > 0:
+    count = 0
+    for t in [t for t in tracks if len(t.hits)>=3]:
+      plt.plot(
+        [h[x] for h in t.hits],
+        [hit_phi(h[phix], h[phiy]) for h in t.hits],
+        color=colors[count % len(colors)],
+        alpha=0.4,
+        linewidth=1
+      )
+      plt.scatter(
+        [h[x] for h in t.hits],
+        [hit_phi(h[phix], h[phiy]) for h in t.hits],
+        color=colors[count % len(colors)],
+        s=2*scale
+      )
+      count+=1
+
+  else:
+    plt.scatter(
+      [h[x] for h in event.hits],
+      [hit_phi(h[phix], h[phiy]) for h in event.hits],
+      color=default_color,
+      s=2*scale
+    )
+
+  plt.title(filename)
+  plt.tick_params(axis='both', which='major', labelsize=4*scale)
+  plt.xlabel(f'{ntox[x]}', fontdict={'fontsize': 4*scale})   # TODO: label "Z"?
+  plt.ylabel("φ", fontdict={'fontsize': 4*scale}, rotation='horizontal')
+
+  if save_to_file:
+    plt.savefig(filename + ".png", bbox_inches='tight', pad_inches=0.2)
+    plt.close()
+  else:
+    plt.show()
+
+
+# def print_event_3d_2phi(event, filename='event_3d_2phi'):
+#   fig = plt.figure(figsize=(16*plotscale, 9*plotscale))
+#   ax = plt.axes(projection='3d')
+#
+#   ax.scatter3D(
+#     [h[x] for h in event.hits],
+#     [h[y] for h in event.hits],
+#     [hit_phi(h, h.x, h.y) for h in event.hits],
+#     color=default_color,
+#     s=2*scale
+#   )
+
+def print_event_3d_phi(event, tracks, x=2, y=0, track_color=0, rotate=False, filename='event_3d_phi'):
+  fig = plt.figure(figsize=(16*plotscale, 9*plotscale))
+  ax = plt.axes(projection='3d')
+
+  ax.scatter3D(
+    [h[x] for h in event.hits],
+    [h[y] for h in event.hits],
+    [hit_phi(h.x, h.y) for h in event.hits],
     color=default_color,
     s=2*scale
   )
 
-  # for t in [t for t in tracks if len(t.hits)==3]:
-  #   plt.plot(
-  #     [h[2] for h in t.hits],
-  #     [hit_phi(h) for h in t.hits],
-  #     color=colors[track_color],
-  #     linewidth=1
-  #   )
+  for t in [t for t in tracks if len(t.hits)>=3]:
+    ax.plot3D(
+      [h[x] for h in t.hits],
+      [h[y] for h in t.hits],
+      [hit_phi(h.x, h.y) for h in t.hits],
+      color=colors[track_color],
+      linewidth=1
+    )
 
   plt.tick_params(axis='both', which='major', labelsize=4*scale)
-  plt.xlabel("Z", fontdict={'fontsize': 4*scale})   # TODO: label "Z"?
-  plt.ylabel("φ", fontdict={'fontsize': 4*scale}, rotation='horizontal')
+  ax.set_xlabel(f'{ntox[x]}', fontdict={'fontsize': 4*scale})
+  ax.set_ylabel(f'{ntox[y]}', fontdict={'fontsize': 4*scale})
+  ax.set_zlabel("φ", fontdict={'fontsize': 4*scale}, rotation='horizontal')
 
   plt.savefig(filename + ".png", bbox_inches='tight', pad_inches=0.2)
-  plt.savefig(filename + ".pdf", transparent=True, bbox_inches='tight', pad_inches=0.2)
+
+  if rotate:
+    for angle in range(0, 360):
+      ax.view_init(10, angle)
+      plt.draw()
+      plt.pause(.001)
 
   plt.close()
