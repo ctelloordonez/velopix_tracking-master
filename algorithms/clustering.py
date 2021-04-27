9249fef397dea7d61fe171e4be8658a6e4398068
Carlos Tello Ordoñez <charlietello97@gmail.com>
2021-04-27 09:35:14 +0200

    Changed phi modification in polar coordinates method

--- algorithms/clustering.py
+++ algorithms/clustering.py
@@ -6,19 +6,19 @@ class Clustering:
         self.bins_count = K
         self.bins = [[] for i in range(self.bins_count)]
 
-    def get_bins(self, event):
+    def get_angle_bins(self, event, x=0, y=1):
         for hit in event.hits:
-            hit_r, hit_phi = get_polar_coordinates(hit.x, hit.y)
-            for b, bin in enumerate(self.bins):
-                if b * (2 * math.pi / self.bins_count) < abs(hit_phi) <= (b+1) * (2 * math.pi / self.bins_count):
-                    bin.append(hit)
+            hit_r, hit_phi = get_polar_coordinates(hit[x], hit[y])
+            if hit_phi < 0:
+                hit_phi = math.pi - hit_phi
+            for b, cluster_bin in enumerate(self.bins):
+                if b * (2 * math.pi / self.bins_count) < hit_phi <= (b+1) * (2 * math.pi / self.bins_count):
+                    cluster_bin.append(hit)
         return self.bins
 
 
 def get_polar_coordinates(x, y):
     r = math.sqrt(x ** 2 + y ** 2)
     phi = math.atan2(x, y)
-    if (phi < 0):
-        phi = math.pi - phi
     return r, phi
 
