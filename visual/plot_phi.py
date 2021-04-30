331c11000b67e881f9c904def60e247d625ea34a
Carlos Tello Ordoñez <charlietello97@gmail.com>
2021-04-30 10:03:35 +0200

    Added search by phi to phi visualization

--- visual/plot_phi.py
+++ visual/plot_phi.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
 import event_model.event_model as em
+import numpy as np
 import validator.validator_lite as vl
 import json
 
 # Solvers
 from algorithms.graph_dfs import graph_dfs
 from algorithms.track_following import track_following
+from algorithms.search_by_phi import SearchByPhi
+
 solutions = {}
 
 # Get an event
-f = open("../events/minibias/velo_event_0.json")
+f = open("../events/bsphiphi/velo_event_0.json")
 json_data = json.loads(f.read())
 event = em.event(json_data)
 f.close()
@@ -32,11 +35,27 @@ for s_number in range(0, event.number_of_modules):
 # solutions["dfs"] = dfs.solve(event)
 
 from visual.print_phi import print_event_2d_phi, print_event_3d_phi, \
-    print_event_2d_2phi, print_event_2d_phi_r, print_event_2d_2phi_r
+    print_event_2d_2phi, print_event_2d_phi_r, print_event_2d_2phi_r, print_event_3d_3phi, plot_projection
 from data_analysis.events_analysis import tracks_from_data
 
 save_to_file = True
 tracks = tracks_from_data(json_data, only_reconstructible=True)
+# tracks = tracks[:100]
+hits = []
+for t in tracks:
+    hits.extend(h for h in event.hits if h in t)
+# print(len(hits))
+search_by_phi = SearchByPhi(event.hits)
+found_tracks = search_by_phi.solve()
+solution = []
+solution.append(found_tracks)
+validation_data = []
+validation_data.append(json_data)
+vl.validate_print(validation_data, solution)
+# print(len(tracks[:100]))
+# print(len(found_tracks))
+plot_projection(event, tracks, filename='test_full', save_to_file=True)
+plot_projection(event, found_tracks, filename='test', save_to_file=True)
 
 # print_event_2d_phi(event, tracks, x=2, phix=0, phiy=1, filename="minibias event0 phi by z", save_to_file=True)
 # print_event_2d_phi(event, tracks, x=0, phix=1, phiy=2, filename="minibias event0 theta by x", save_to_file=True)
@@ -47,10 +66,10 @@ tracks = tracks_from_data(json_data, only_reconstructible=True)
 # print_event_2d_phi_r(event, tracks, phix=1, phiy=2, filename="minibias event0 theta by r", save_to_file=True)
 # print_event_2d_phi_r(event, tracks, phix=2, phiy=0, filename="minibias event0 psi by r", save_to_file=True)
 
-print_event_2d_2phi(event, tracks, phix=[0,1], phiy=[1,2], filename="phi-theta", save_to_file=save_to_file)
-print_event_2d_2phi(event, tracks, phix=[1,2], phiy=[2,0], filename="theta-psi", save_to_file=save_to_file)
-print_event_2d_2phi(event, tracks, phix=[2,0], phiy=[0,1], filename="psi-phi", save_to_file=save_to_file)
-print_event_2d_2phi(event, tracks, phix=[0,1], phiy=[2,0], filename="phi-psi", save_to_file=save_to_file)
+# print_event_2d_2phi(event, tracks, phix=[0,1], phiy=[1,2], filename="phi-theta", save_to_file=save_to_file)
+# print_event_2d_2phi(event, tracks, phix=[1,2], phiy=[2,0], filename="theta-psi", save_to_file=save_to_file)
+# print_event_2d_2phi(event, tracks, phix=[2,0], phiy=[0,1], filename="psi-phi", save_to_file=save_to_file)
+# print_event_2d_2phi(event, tracks, phix=[0,1], phiy=[2,0], filename="phi-psi", save_to_file=save_to_file)
 
 # print_event_2d_2phi_r(event, tracks, phix=0, phiy=1, x=2, filename="phi by angle(r, z)", save_to_file=save_to_file)
 # print_event_2d_2phi_r(event, tracks, phix=1, phiy=2, x=0, filename="theta by angle(r, x)", save_to_file=save_to_file)
@@ -59,3 +78,8 @@ print_event_2d_2phi(event, tracks, phix=[0,1], phiy=[2,0], filename="phi-psi", s
 # print_event_3d_phi(event, tracks, x=0, y=1, phix=0, phiy=1, filename='minibias event0 phi by x-y', save_to_file=True)
 # print_event_3d_phi(event, tracks, x=1, y=2, phix=1, phiy=2, filename='minibias event0 theta by y-z', save_to_file=True)
 # print_event_3d_phi(event, tracks, x=2, y=0, phix=2, phiy=0, filename='minibias event0 psi by z-x', save_to_file=True)
+
+# for o in range(-40, 101, 20):
+#     print_event_3d_3phi(event, tracks, offset=o, filename=f'3d 3phi {o}', save_to_file=False)
+
+# plot_projection(event, tracks, filename='projectionphi_phi', save_to_file=False)
