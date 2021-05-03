32aa30bd33ea6db8ed0c8ada5817fdce588e4206
TK-97 <57396997+TK-97@users.noreply.github.com>
2021-05-03 17:24:54 +0200

    Update search_by_constant.py

--- algorithms/search_by_constant.py
+++ algorithms/search_by_constant.py
@@ -15,9 +15,34 @@ class SearchByConstant:
         skipped = 0 # variable to keep track of the number of modules skipped
 
         for h in self.hits: # loop over the hits in the event
-            if abs(math.atan2(h.y, h.x) - math.atan2(current_track[0].y, current_track[0].x)) < 0.01: # if the hits polar angle is withing range of the track
-                # We skipped 1 module in the track already, hit is in module after the first hit of the track
-                if skipped == 1 and h.module_number == current_track[0].module_number + 2: 
+            # if the hits polar angle is withing range of the last hit in the track ( rather than first in first implementation)
+            if abs(math.atan2(h.y, h.x) - math.atan2(current_track[-1].y, current_track[-1].x)) < 0.01: 
+                # put the hits apart if track hits are too far apart
+                if 0 <abs(h.module_number - current_track[0].module_number) < 13 : 
+                    if(len(current_track) >= 2 and current_track[0].module_number < h.module_number < current_track[-1].module_number):
+                        if(checkMonotonicity(current_track[0], h, current_track[-1]) and abs(calculateConstant(current_track[0] , h ) - calculateConstant( h,current_track[-1])) < 0.01):
+                            # add hit to current track
+                        else:
+                            # check other track, add to new track
+                    elif(current_track[0].module_number > h.module_number):
+                        if(len(current_track) >= 2):
+                            if(checkMonotonicity(h,current_track[0] , current_track[-1]) and abs(calculateConstant(current_track[0] , h ) - calculateConstant( h,current_track[-1])) < 0.01):
+                                # add hit to current track
+                            else:
+                                # check other track, add to new track
+                    elif(h.module_number > current_track[-1].module_number):
+                        if(len(current_track) >= 2):
+                            if(checkMonotonicity(current_track[0], current_track[-1],h ) and abs(calculateConstant(current_track[0] , h ) - calculateConstant( h,current_track[-1])) < 0.01):
+                                # add hit to track
+                            else:
+                                # check other track, add to new track
+                elif(0 == abs(h.module_number - current_track[0].module_number)):
+                    # do not add hit to track
+            else: 
+                # add this hit to another track
+
+
+
                     current_track.insert(0, h) # add the hit after the first track hit
                 # We skipped 1 module in the track already, hit is in module before the first hit of the track
                 elif skipped == 1 and h.module_number == current_track[-1].module_number - 2:
@@ -71,6 +96,21 @@ def sort_by_moduleNumber(hits):
 def calculateConstant(hit1,hit2):
     return math.sqrt((hit1.x-hit2.x)^2+(hit1.y+hit2.y)^2)/math.sqrt((hit1.z-hit2.z)^2)
 
+def checkMonotonicity(hit1,hit2,hit3):
+    if(hit1.pol_r < hit2.pol_r):
+        if(hit2.pol_r <= hit3.pol_r):
+            return True
+        else:
+            return False
+
+    if(hit1.pol_r > hit2.pol_r):
+        if(hit2.pol_r >= hit3.pol_r):
+            return True
+        else:
+            return False
+    else:
+        return True
+
 #     def solve(self):
 #         tracks = [] # list of tracks found
 #         current_track = [] # list of hits representing the track currently under consideration
@@ -78,7 +118,8 @@ def calculateConstant(hit1,hit2):
 #         self.hits = self.hits[1:] # Take the first hit out of consideration for forming a track with itself
 #         skipped = 0 # variable to keep track of the number of modules skipped
 #         for h in self.hits: # loop over the hits in the event
-#             if abs(math.atan2(h.y, h.x) - math.atan2(current_track[0].y, current_track[0].x)) < 0.01: # if the hits polar angle is withing range of the track
+#           # if the hits polar angle is withing range of the last hit in the track ( rather than first in first implementation)
+            # if abs(math.atan2(h.y, h.x) - math.atan2(current_track[-1].y, current_track[-1].x)) < 0.01: 
 #                 # We skipped 1 module in the track already, hit is in module after the first hit of the track
 #                 if skipped == 1 and h.module_number == current_track[0].module_number + 2: 
 #                     current_track.insert(0, h) # add the hit after the first track hit
