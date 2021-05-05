433bfd583b3546ffb2c56d3fe2e0f4e840c963a5
TK-97 <57396997+TK-97@users.noreply.github.com>
2021-05-05 13:20:25 +0200

    Update search_by_constant.py

--- algorithms/search_by_constant.py
+++ algorithms/search_by_constant.py
@@ -43,38 +43,11 @@ class SearchByConstant:
 
 
 
-                    current_track.insert(0, h) # add the hit after the first track hit
-                # We skipped 1 module in the track already, hit is in module before the first hit of the track
-                elif skipped == 1 and h.module_number == current_track[-1].module_number - 2:
-                    current_track.append(h) # add the hit at the end of the track
-                # We did not yet skip a module in the current track,  hit is in module before the first hit of the track
-                elif h.module_number == current_track[0].module_number - 2:
-                    current_track.insert(0, h) # add the hit after the first track hit
-                # We did not yet skip a module in the current track,  hit is in module after the last hit of the track
-                elif h.module_number == current_track[-1].module_number + 2:
-                    current_track.append(h) # add the hit at the end of the track
-                # We did not yet skip a module in the current track,  hit is 2 modules before the first hit of the track
-                elif skipped <= 0 and h.module_number == current_track[0].module_number - 4:
-                    current_track.insert(0, h) # add the hit 
-                    skipped += 1 # increase skipped module counter
-                # We did not yet skip a module in the current track,  hit is 2 modules after the last hit of the track
-                elif skipped <= 0 and h.module_number == current_track[-1].module_number + 4:
-                    current_track.append(h) # add the hit, skip next module counter
-                    skipped += 1 # increase skipped module counter
-                else:
-                    skipped = 0 # reset skipped modules counnter
-                    if len(current_track) > 1: # if the current track has a minimal length
-                        tracks.append(em.track(current_track)) # add it to the list of tracks found
-                    current_track = [] # Reset current track
-                    current_track.append(h) # Add the hit as the initial hit of the track
-            else: # if hit h is not sufficiently close to the current track
-                skipped = 0 # reset skipped counter
-                if len(current_track) > 1: # If the current track has a minimal number of hits
-                    tracks.append(em.track(current_track)) # add it to the list of tracks found
-                current_track = [] # Reset current track
-                current_track.append(h) # Add the hit as the initial hit of the track
-        if len(current_track) > 1:
-            tracks.append(em.track(current_track)) # append final track if longer than 1
+                #     current_track.insert(0, h) # add the hit after the first track hit
+                # # We skipped 1 module in the track already, hit is in module before the first hit of the track
+                # elif skipped == 1 and h.module_number == current_track[-1].module_number - 2:
+                #     current_track.append(h) # add the hit at the end of the track
+             
 
         return tracks
 
