e6224373ddfbf48829273bc8dff9cdd6ca459a96
Carlos Tello Ordoñez <charlietello97@gmail.com>
2021-05-15 09:05:40 +0200

    Add solve 4 method to search by phi

--- algorithms/search_by_phi.py
+++ algorithms/search_by_phi.py
@@ -7,6 +7,7 @@ class SearchByPhi:
     def __init__(self, event):
         self.hits = sort_by_phi(event.hits) # sort hits by phi
         self.acceptance = 0.01
+        self.min_track_length = 2
 
     def solve(self):
         tracks = [] # list of tracks found
@@ -44,11 +45,11 @@ class SearchByPhi:
                     current_track.append(h) # Add the hit as the initial hit of the track
             else: # if hit h is not sufficiently close to the current track
                 skipped = 0 # reset skipped counter
-                if len(current_track) > 1: # If the current track has a minimal number of hits
+                if len(current_track) >= self.min_track_length: # If the current track has a minimal number of hits
                     tracks.append(em.track(current_track)) # add it to the list of tracks found
                 current_track = [] # Reset current track
                 current_track.append(h) # Add the hit as the initial hit of the track
-        if len(current_track) > 1:
+        if len(current_track) >= self.min_track_length:
             tracks.append(em.track(current_track)) # append final track if longer than 1
 
         return tracks
@@ -86,12 +87,12 @@ class SearchByPhi:
                     skipped += 1
                     current_track.append(h)
                 else:
-                    if len(current_track) > 2:
+                    if len(current_track) >= self.min_track_length:
                         tracks.append(em.track(current_track))
                     skipped = 0
                     current_track = [h]
 
-            if len(current_track) > 2:
+            if len(current_track) >= self.min_track_length:
                 tracks.append(em.track(current_track))
 
         return tracks
@@ -133,15 +134,80 @@ class SearchByPhi:
                     skipped += 1
                     current_track.append(h)
                 else:
-                    if len(current_track) > 2:
+                    if len(current_track) >= self.min_track_length:
                         tracks.append(em.track(current_track))
                     skipped = 0
                     current_track = [h]
 
-            if len(current_track) > 2:
+            if len(current_track) >= self.min_track_length:
                 tracks.append(em.track(current_track))
 
         return tracks
+
+    def solve4(self):
+        grouped_by_phi = []
+        g_track = []
+        g_track_phi_sum = 0
+        for h in self.hits:
+            if len(g_track) <= 0:
+                g_track.append(h)
+                g_track_phi_sum += math.atan2(h.y, h.x)
+            elif check_acceptance(h, g_track, g_track_phi_sum, self.acceptance): # if the hits polar angle is withing range of the track
+                g_track.append(h)
+                g_track_phi_sum += math.atan2(h.y, h.x)
+            else:
+                grouped_by_phi.append(g_track)
+                g_track = []
+                g_track_phi_sum = 0
+        if len(g_track) > 0:
+            grouped_by_phi.append(g_track)
+
+        tracks = []
+        for g_track in grouped_by_phi:
+            sorted_by_module = sort_by_module(g_track)
+            current_track = []
+            skipped = 0
+            for h in sorted_by_module:
+                if len(current_track) <= 0:
+                    current_track.append(h)
+                    continue
+                if h.module_number == current_track[-1].module_number:
+                    continue
+                if h.module_number == current_track[-1].module_number + 2 or \
+                        h.module_number == current_track[-1].module_number + 1:
+                    current_track.append(h)
+                elif skipped < 1 and (h.module_number == current_track[-1].module_number + 4 or
+                                      h.module_number == current_track[-1].module_number + 3):
+                    skipped += 1
+                    current_track.append(h)
+                else:
+                    if len(current_track) >= self.min_track_length:
+                        tracks.append(em.track(current_track))
+                    skipped = 0
+                    current_track = [h]
+
+            if len(current_track) >= self.min_track_length:
+                tracks.append(em.track(current_track))
+
+        return tracks
+
+
+def check_acceptance(hit, track, t_phi_sum, acceptance):
+    deviation_sum = 0
+    mean = t_phi_sum / len(track)
+    for h in track:
+        deviation_sum += (math.atan2(h.y, h.x) - mean)**2
+
+    variance = deviation_sum / len(track)
+    s_deviation = math.sqrt(variance)
+
+    candiate_deviation = (math.atan2(hit.y, hit.x) - mean)**2
+    return math.sqrt((candiate_deviation - s_deviation)**2) < 0.002
+    # rss = 0
+    # for h in track:
+    #     rss += (math.atan2(hit.y, hit.x) - math.atan2(h.y, h.x))**2
+    # mrss = rss / len(track)
+    # return math.sqrt(mrss) < acceptance
 
 
 def sort_by_phi(hits):
