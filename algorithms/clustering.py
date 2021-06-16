4e5c8d7876fc74a82cd456a54fd8fe1becf1fa3c
Carlos Tello Ordoñez <charlietello97@gmail.com>
2021-06-16 12:26:24 +0200

    Merged clustering algorithms in single file
    Clustering class has a common initialization for all the clustering methods. There's one 'solve_...' method for each clustering algorithm

--- algorithms/clustering.py
+++ algorithms/clustering.py
@@ -1,40 +1,150 @@
 import math
 
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering, AffinityPropagation, MeanShift
+from event_model import event_model as em
+
 
 class Clustering:
-    def __init__(self, K=4):
-        self.bins_count = K
-        self.bins = [[] for i in range(self.bins_count)]
+    def __init__(self, event):
+        self.hits_a = [hit_angles(h) for h in event.hits]
 
-    def get_angle_bins(self, event, x=0, y=1):
-        for hit in event.hits:
-            hit_r, hit_phi = get_polar_coordinates(hit[x], hit[y])
-            if hit_phi < 0:
-                hit_phi = math.pi - hit_phi
-            for b, cluster_bin in enumerate(self.bins):
-                if b * (2 * math.pi / self.bins_count) < hit_phi <= (b+1) * (2 * math.pi / self.bins_count):
-                    cluster_bin.append(hit)
-        return self.bins
+        self.hits = event.hits
 
-    def get_space_bins(self, event, coordinate=2):
-        for hit in event.hits:
-            if coordinate == 2:
-                offset = 400
-            else:
-                offset = 40
-            value = hit[coordinate] + offset
-            if coordinate == 2:
-                space_range = 1200
-            else:
-                space_range = 80
-            for b, cluster_bin in enumerate(self.bins):
-                if b * (space_range / self.bins_count) < value <= (b+1) * (space_range / self.bins_count):
-                    cluster_bin.append(hit)
-        return self.bins
+    def solve_agglomerative_clustering(self, linkage='single', distance_threshold=0.009):
+        distance_matrix = compute_distances(self.hits)
+        # clusterer = AgglomerativeClustering(n_clusters=None, affinity='l1', linkage=linkage,
+        #                                     distance_threshold=distance_threshold).fit(self.hits_a)
+        clusterer = AgglomerativeClustering(n_clusters=None, affinity='precomputed', linkage='complete',
+                                            distance_threshold=0.01)
+        clusterer.fit_predict(distance_matrix)
+
+        return self.tracks_from_clusterer(clusterer)
+
+    def solve_mean_shift(self):
+        clusterer = MeanShift(bandwidth=0.009, bin_seeding=False, min_bin_freq=3, cluster_all=False, max_iter=1000).fit(self.hits_a)
+        return self.tracks_from_clusterer(clusterer)
+
+    # def solve_hdbscan(self):
+    #     distance_matrix = compute_distances(self.hits)
+    #     clusterer = hdbscan.HDBSCAN(metric='precomputed',min_cluster_size=3)
+    #     clusterer.fit(distance_matrix)
+    #
+    #     return self.tracks_from_clusterer(clusterer)
+
+    def solve_affinity_propagation(self):
+        distance_matrix = compute_distances(self.hits)
+        clusterer = AffinityPropagation(affinity='precomputed', random_state=None) # affinity='precomputed'
+        clusterer.fit_predict(distance_matrix)
+
+        return self.tracks_from_clusterer(clusterer)
+
+    def tracks_from_clusterer(self, clusterer):
+        tracks = []
+        for t in range(clusterer.labels_.max() + 1):
+            currentTrack = []
+            for k in range(len(self.hits)):
+                if clusterer.labels_[k] == t:
+                    currentTrack.append(self.hits[k])
+            tracks.append(em.track(currentTrack))
+
+        return tracks
 
 
-def get_polar_coordinates(x, y):
-    r = math.sqrt(x ** 2 + y ** 2)
-    phi = math.atan2(x, y)
-    return r, phi
+def hit_angles(hit):
+    shift=-0
+    # return [math.atan2(hit.y, hit.x), hit.x, hit.y]
+    return [math.atan2(hit.y, hit.x),
+            math.atan2(hit.z+shift, hit.y),
+            math.atan2(hit.x, hit.z+shift)]
 
+
+def compute_distances(hits):
+    distanceMatrix = np.empty((len(hits), len(hits)))
+    for i in range(len(hits)):
+        for j in range(i, len(hits)):
+            distanceMatrix[i][j] = distance(hits[i], hits[j])
+            distanceMatrix[j][i] = distanceMatrix[i][j]
+    return distanceMatrix
+
+
+def distance(hit1, hit2):
+    if hit1.module_number == hit2.module_number:
+        return 10
+    elif (hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number != hit2.module_number:
+        return abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x))
+    else:
+        return 1
+        # abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x))*abs(hit1.module_number - hit2.module_number)
+
+
+def distance2(hit1, hit2):
+    if hit1.module_number == hit2.module_number:
+        return 10
+    elif (hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number != hit2.module_number:
+        return (abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x))) / math.sqrt(hit1.x ** 2 + hit2.x ** 2)
+    else:
+        return 1
+        # abs(math.ata
+
+
+def distance3(hit1, hit2):
+    if hit1.module_number == hit2.module_number:
+        return 10
+    elif (hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number != hit2.module_number:
+        return (abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x))) / math.sqrt(hit1.z ** 2 + hit2.z ** 2)
+    else:
+        return 1
+
+
+def distance4(hit1,hit2):
+    if hit1.module_number == hit2.module_number:
+        return 100000
+    elif((hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number !=  hit2.module_number):
+        return 10000*(abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))+ abs(hit1.module_number - hit2.module_number)
+    else:
+        return 50000*(abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))+ abs(hit1.module_number - hit2.module_number)
+
+
+def distance5(hit1,hit2):
+    if math.atan2(hit2.y, hit2.x) - math.atan2(hit1.y, hit1.x)<0.01:
+
+        if((hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number !=  hit2.module_number and abs(hit1.module_number - hit2.module_number) < 10):
+            return abs(hit1.module_number - hit2.module_number)
+        else:
+            return 500
+    else:
+        return 500
+
+
+def distance6(hit1,hit2):
+    if hit1.module_number ==  hit2.module_number:
+        return 100000
+    elif((hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number !=  hit2.module_number):
+        return (abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))/(2*math.pi/20000) + abs(hit1.module_number - hit2.module_number)
+    else:
+        return 10000
+
+
+def distance7(hit1,hit2):
+    if hit1.module_number ==  hit2.module_number:
+        return 100000
+    elif (hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number !=  hit2.module_number:
+        if(hit1.module_number >  hit2.module_number):
+            return 500*(abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))/(polarDistance(hit1)-polarDistance(hit2)+1e-7)
+        else:
+            return 500*(abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))/(polarDistance(hit2)-polarDistance(hit1)+1e-7)
+    else:
+        return 10000
+
+
+def distance8(hit1,hit2):
+    if hit1.module_number ==  hit2.module_number:
+        return 100000
+    elif (hit1.module_number % 2) == (hit2.module_number % 2) and hit1.module_number !=  hit2.module_number:
+        if(hit1.module_number >  hit2.module_number):
+            return 500*(abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))/(polarDistance(hit1)-polarDistance(hit2)+1e-7)
+        else:
+            return 500*(abs(math.atan2(hit1.y, hit1.x) - math.atan2(hit2.y, hit2.x)))/(polarDistance(hit2)-polarDistance(hit1)+1e-7)
+    else:
+        return 10000
